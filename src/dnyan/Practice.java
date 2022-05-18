8f87d5b7baf5e369d105de023df0b39fe6a8c95d
DnyaneshwariPanchal <dnyaneshwaripanchal@gmail.com>
2022-05-18 23:42:30 +0530

    chnages done

--- src/dnyan/Practice.java
+++ src/dnyan/Practice.java
@@ -1,6 +1,13 @@
 package dnyan;
 
 public class Practice {
-    public static void main(String[] args) {
-    	System.out.println("project created");    }
+ void add(int num1,int num2) {
+	 System.out.println("sum is "+(num1+num2));
+ }
+	// 0,1,1,2,3,5,8,13,21,34,55,89
+	public static void main(String[] args) {
+		System.out.println("project created");
+		Practice practice=new Practice();
+		practice.add(2,10);
+	}
 }
